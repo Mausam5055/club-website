7ad88fd1e17b7c2d85321d5a32f126a891a4d6e0
Mausam5055 <rikikumkar@gmail.com>
2024-08-23 01:22:25 +0530

    refactor: Remove unused imports and organize code in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
-import Image from "next/image";
 
 
-export default function Home() {
+import CaruselUsage from '@/components/component/frontend/CaruselUsage'
+import Footer from '@/components/component/frontend/footer/page'
+// import Footer from '@/components/component/frontend/footer'
+import Hero from '@/components/component/frontend/Hero'
+import ImgeSlider from '@/components/component/frontend/ImgeSlider'
+import Navbar from '@/components/component/frontend/page'
+import Section4 from '@/components/component/frontend/section4'
+import Section5 from '@/components/component/frontend/section5'
+import Section6 from '@/components/component/frontend/section6'
+import React from 'react'
+
+export default function page() {
   return (
-    <div className="flex items-center justify-center flex-col min-h-screen">
-      <h2 className="text-4xl"> Welcome to Linpack</h2>
+    <div className=''>
+      <Navbar />
+      <Hero/>
+      <CaruselUsage />
+      <ImgeSlider />
+      <Section5 />
+      <Section6 />
+      <Section4/>
+      <Footer/> 
     </div>
-  );
+  )
 }
