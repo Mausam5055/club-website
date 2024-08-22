dc30202f6556159271dafe81ed054faf9c8ddbaf
Mausam5055 <rikikumkar@gmail.com>
2024-08-23 01:13:15 +0530

    refactor: Comment out unused import of NewComp in page.tsx

--- app/(frontend)/test/page.tsx
+++ app/(frontend)/test/page.tsx
@@ -1,5 +1,5 @@
 import Googlemap from '@/components/component/frontend/googlemap'
-import NewComp from '@/components/component/frontend/newComp'
+// import NewComp from '@/components/component/frontend/newComp'
 import React from 'react'
 
 export default function page() {
