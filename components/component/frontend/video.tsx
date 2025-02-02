4a897d4a13ea34c2bc3eb9a8cc850f57f2f36b55
Mausam5055 <rikikumkar@gmail.com>
2025-02-02 23:53:15 +0530

    feat: Enhance Video component with preload and playsInline attributes

--- components/component/frontend/video.tsx
+++ components/component/frontend/video.tsx
@@ -1,8 +1,15 @@
 export function Video() {
-    return (
-      <video width="320" height="240" className="w-full h-full object-cover" controls preload="none">
-        <source src="/video/linpack.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    )
-  }
+  return (
+    <video
+      width="320"
+      height="240"
+      className="w-full h-full object-cover"
+      controls
+      preload="auto"
+      playsInline
+    >
+      <source src="/video/linpack.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+}
