9238bc7218059a1746f812b468a72dbce000001a
Mausam5055 <rikikumkar@gmail.com>
2025-02-03 01:30:06 +0530

    debug

--- components/component/frontend/video.tsx
+++ components/component/frontend/video.tsx
@@ -19,7 +19,7 @@ export default function Video({
         className={cn("w-full h-full object-cover", className)}
         {...props}
       >
-        <source src={"/video/Linpack.mp4"} type="video/webm" />
+        <source src={"/video/Linpack.mp4"} type="video/mp4" />
         Your browser does not support HTML5 video.
       </video>
   );
