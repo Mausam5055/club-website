97850097681a780eb12b9add2438ef2b66f39a5a
Mausam5055 <rikikumkar@gmail.com>
2025-02-03 07:58:22 +0530

    debug

--- components/component/frontend/video.tsx
+++ components/component/frontend/video.tsx
@@ -7,19 +7,18 @@ export default function Video({
 }: {
   className?: string;
 }) {
-  const [muted, setMuted] = useState(true);
 
   return (
       <video
         autoPlay
         playsInline
-        muted={muted}
+        controls
         loop
         id="myVideo"
         className={cn("w-full h-full object-cover", className)}
         {...props}
       >
-        <source src={"/video/Linpack.mp4"} type="video/mp4" />
+        <source src={"/video/Linpack.webm"} type="video/webm" />
         Your browser does not support HTML5 video.
       </video>
   );
